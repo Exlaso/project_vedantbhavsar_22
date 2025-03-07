1a08c03d38e995c05bbcf39a4ba69e2631f999ac
KrishnaCodez <urvishpvt@gmail.com>
2025-03-07 11:56:35 +0530

    onboarding form

---
+++ app/(dashboard)/onboarding/page.tsx
@@ -0,0 +1,9 @@
+import OnboardingForm from "@/components/forms/OnboardingForm";
+
+export default async function OnboardingPage() {
+  return (
+    <div>
+      <OnboardingForm />
+    </div>
+  );
+}
