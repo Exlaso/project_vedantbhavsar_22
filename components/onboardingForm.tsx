83a02b10873e10ac49fb10c38212095d212b4179
Exlaso <vedantbhavsar67@gmail.com>
2025-03-07 17:12:58 +0530

    optimized the onboarding flow

--- components/onboardingForm.tsx
+++ components/onboardingForm.tsx
@@ -20,55 +20,59 @@ function createZodEnum<T extends string[]>(values: T) {
     return e;
 }
 
-export function OnboardingForm({questions,currentIDX}: { questions: Tables<'general_questions'>[],currentIDX:number }) {
+export function OnboardingForm({questions, currentIDX}: {
+    questions: Tables<'general_questions'>[],
+    currentIDX: number
+}) {
     const router = useRouter();
-    const {data: currentIndex, refetch} = useQuery({
-        queryKey: ["Current Index"],
-        queryFn: getCurrentProgress,
-        enabled:false,
-        initialData: () => {return currentIDX}
-    })
-    console.log({currentIndex})
+    const [index, setIndex] = useState<number>(currentIDX);
     const supabase = getSupabaseClient()
-    const {mutate, isPending,error} = useMutation({
-        mutationFn: async (values:Record<string, string | undefined>) => {
+    const {mutate, isPending, error} = useMutation({
+        mutationFn: async (values: Record<string, string | undefined>) => {
             const main_ans = Object.values(values)
-            const data=  await supabase.from('user_answers').upsert({
+            const data = await supabase.from('user_answers').upsert({
                 answer: main_ans?.at(0),
                 follow_up_answer: main_ans?.at(1),
                 question_id: question?.id || 0
-            }).select("*")
+            });
             if (data.error) throw data.error;
             return data.data
         },
         onSuccess: async () => {
-            const nextIndex= (currentIndex|| 0)+1
+            const nextIndex = (
+                index || 0
+            ) + 1
             const isLast = nextIndex >= questions.length
-            await updateProgress(nextIndex,isLast)
+            updateProgress(nextIndex, isLast).then()
+
             if (isLast) {
                 router.push("/dashboard")
-            }else{
-            await refetch()
+            } else {
+                setIndex(nextIndex)
             }
 
         }
     })
     const [choosen_answer, setChoosen_answer] = useState<string>("");
-    const question = questions.at(currentIndex || 0)
+    const question = questions.at(index || 0)
     if (!question) {
         return <div>
-             Something went wrong.
+            Something went wrong.
         </div>
     }
     const shouldFollowup = question?.follow_up_conditions?.includes(question.options.indexOf(choosen_answer)) || false
-    console.log({shouldFollowup,question})
+    console.log({shouldFollowup, question})
     const schema = new ZodProvider(z.object({
         [`${question.id}: ${question?.question}`]: createZodEnum(question?.options || []),
-        ...(shouldFollowup ? {[`${question.follow_up_question}`]: z.string().optional().superRefine(fieldConfig({fieldType:"textArea"}))} : {})
+        ...(
+            shouldFollowup ? {[`${question.follow_up_question}`]: z.string().optional().superRefine(fieldConfig({fieldType: "textArea"}))} : {}
+        )
     }))
     return <CardContent className="">
         <div className={"flex "}><AutoForm
-            onSubmit={values => {mutate(values)}}
+            onSubmit={values => {
+                mutate(values)
+            }}
             schema={schema} onFormInit={form => {
             form.watch(values => {
                 setChoosen_answer(values[`${question.id}: ${question?.question}`] || "0");
@@ -78,7 +82,7 @@ export function OnboardingForm({questions,currentIDX}: { questions: Tables<'gene
 
         >
             <Button disabled={isPending} type="submit" className={"flex justify-center items-center w-full gap-2"}>
-                {isPending&& <Loader2 className={"animate-spin"}/> } Submit
+                {isPending && <Loader2 className={"animate-spin"}/>} Submit
             </Button>
             <span>{error?.message}</span>
         </AutoForm>
