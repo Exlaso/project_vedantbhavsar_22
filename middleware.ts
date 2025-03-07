e781123138f0e357f2f7337e1e3ee36298affcdb
KrishnaCodez <urvishpvt@gmail.com>
2025-03-07 16:39:39 +0530

    refactor: clean up middleware code and improve formatting

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,10 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-import {clerkMiddleware, createRouteMatcher} from "@clerk/nextjs/server";
-import {redirect} from "next/navigation";
-
-
-const isPublicRoute = createRouteMatcher(["/","/sign-in","/sign-up"]);
+const isPublicRoute = createRouteMatcher(["/", "/sign-in", "/sign-up"]);
 export default clerkMiddleware(async (auth, request, event) => {
-    if (!(isPublicRoute(request))) await auth.protect()
-})
-
+  if (!isPublicRoute(request)) await auth.protect();
+});
 
 // export default authMiddleware({
 //   // Allow signed out users to access the specified routes:
@@ -17,11 +14,11 @@ export default clerkMiddleware(async (auth, request, event) => {
 // });
 
 export const config = {
-    matcher: [
-        // Exclude files with a "." followed by an extension, which are typically static files.
-        // Exclude files in the _next directory, which are Next.js internals.
-        "/((?!.+\\.[\\w]+$|_next).*)",
-        // Re-include any files in the api or trpc folders that might have an extension
-        "/(api|trpc)(.*)",
-    ],
+  matcher: [
+    // Exclude files with a "." followed by an extension, which are typically static files.
+    // Exclude files in the _next directory, which are Next.js internals.
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    // Re-include any files in the api or trpc folders that might have an extension
+    "/(api|trpc)(.*)",
+  ],
 };
